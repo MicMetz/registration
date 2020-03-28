19d9fc31c54187bbb67f327aa3ba2e0d891a41f3
mik <mime9599@colorado.edu>
2020-03-28 14:04:23 -0600

    finished checking function. Added validation icons.

--- scripts/input_Validation.js
+++ scripts/input_Validation.js
@@ -2,30 +2,33 @@
 
 let report = document.getElementsByClassName("reqsp");
 let target = document.getElementsByClassName("reqfield");
+let targetlabel = document.getElementsByClassName("reqlabel");
 
 function input_Validation() {
+    let checkB4 = 0;
     for (let x = 0; x < target.length; x++) {
         if (target[x].value === "") {
             report[x].style.visibility = "visible";
         }
         else {
             report[x].style.visibility = "hidden";
+            checkB4++;
         }
     }
+    if (checkB4 === target.length) { document.forms["AHGproposalform2020"].submit(); }
 }
 
 function setState() {
-    let numcheck = target.length;
-    let checker = new Array(numcheck).fill(false);
-    let rolling = true;
-
-    while (rolling === true) {
-        for (let i = 0; i < target.length; i++) {
-            if (target[i].value !== "") {
-                report[i].before.style.visibility = "visible";
-            } else {
-                report[i].before.style.visibility = "hidden";
-            }
+    for (let i = 0; i < target.length; i++) {
+        if (target[i].value !== "") {
+            targetlabel[i].classList.add("stay");
+            report[i].style.visibility = "hidden";
+            report[i].classList.add("valid");
+            report[i].classList.remove("invalid");
+        } else {
+            report[i].classList.remove("valid");
+            report[i].classList.add("invalid");
+            targetlabel[i].classList.remove("stay");
         }
     }
 }
