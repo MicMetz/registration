314b3cf077b83ba1b497d3a8956adca8d3ab3a43
mik <mime9599@colorado.edu>
2020-03-27 20:08:02 -0600

    started onload checking function

--- scripts/input_Validation.js
+++ scripts/input_Validation.js
@@ -1,15 +1,31 @@
 
 
+let report = document.getElementsByClassName("reqsp");
+let target = document.getElementsByClassName("reqfield");
 
 function input_Validation() {
-    let report = document.getElementsByClassName("reqsp");
-    let target = document.getElementsByClassName("reqfield");
     for (let x = 0; x < target.length; x++) {
         if (target[x].value === "") {
             report[x].style.visibility = "visible";
         }
         else {
             report[x].style.visibility = "hidden";
+        }
+    }
+}
+
+function setState() {
+    let numcheck = target.length;
+    let checker = new Array(numcheck).fill(false);
+    let rolling = true;
+
+    while (rolling === true) {
+        for (let i = 0; i < target.length; i++) {
+            if (target[i].value !== "") {
+                report[i].before.style.visibility = "visible";
+            } else {
+                report[i].before.style.visibility = "hidden";
+            }
         }
     }
 }
