004aaabbf4fabe528f87672b0ced2b49dd5e41e7
mik <mime9599@colorado.edu>
2020-04-04 23:48:57 -0600

    Fixed radio button validation

--- scripts/input_Validation.js
+++ scripts/input_Validation.js
@@ -1,69 +1,44 @@
-
-
 let report = document.getElementsByClassName("reqsp");
 let target = document.getElementsByClassName("reqfield");
 let targetlabel = document.getElementsByClassName("reqlabel");
-
-let targetRadio = document.getElementsByClassName("reqfield_Radio");
-let reportRadio = document.getElementsByClassName("Radreq");
 
 function input_Validation() {
     let checkB4 = 0;
     for (let x = 0; x < target.length; x++) {
         if (target[x].value === "") {
             report[x].style.visibility = "visible";
-        }
-        else {
+        } else {
             report[x].style.visibility = "hidden";
             checkB4++;
         }
     }
-
-    let count = 0;
-    for (let i = 0; i < targetRadio.length; i++) {
-        if (targetRadio[i].checked === false) {
-
-        } else {
-            count++;
-        }
+    if (checkB4 === target.length) {
+        document.forms["AHGproposalform2020"].submit();
     }
-    if (count === 0) { reportRadio.style.visibility = "visible"; }
-    else { reportRadio.style.visibility = "hidden"; }
-
-    if (checkB4 === target.length) { document.forms["AHGproposalform2020"].submit(); }
 }
 
 function setState() {
+    debugger
     for (let i = 0; i < target.length; i++) {
         if (target[i].value !== "") {
-            targetlabel[i].classList.add("stay");
             report[i].style.visibility = "hidden";
             report[i].classList.add("valid");
             report[i].classList.remove("invalid");
+            if (i === 4) { continue; }
+            else { targetlabel[i].classList.add("stay"); }
         } else {
             report[i].classList.remove("valid");
             report[i].classList.add("invalid");
-            targetlabel[i].classList.remove("stay");
+            if (i === 4) { continue; }
+            else { targetlabel[i].classList.remove("stay"); }
         }
     }
 }
 
-function setStateRadio() {
-    debugger;
-    let count = 0;
-    for (let i = 0; i < targetRadio.length; i++) {
-        if (targetRadio[i].checked === false) {
+function setStateRadio(x) {
+    debugger
+    let targetRadio = document.getElementsByClassName("reqfield")[4];
 
-        } else {
-            count++;
-        }
-        if (count !== 0) {
-            reportRadio.classList.remove("invalid");
-            reportRadio.classList.add("valid");
-        }
-        else {
-            reportRadio.classList.remove("valid");
-            reportRadio.classList.add("invalid");
-        }
-    }
+    targetRadio.value = x;
+    setState();
 }
